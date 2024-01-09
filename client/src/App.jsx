db21b93e10ecc4b959a1a7544235cdc32c05d025
Darian Evans <darian.janelle.evans@gmail.com>
2024-01-09 18:06:22 -0500

    added homepage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
+    <Router>
     <div>
       <TodoList />
+     
+    <Routes>
+    <Route path="/home" element={<Home/>} />
+    </Routes>
     </div>
+    </Router>
   );
 };
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const  Home = () => {
+    return (
+        <div>
+            <h1>
+                Welcome to my ToDo List Application
+            </h1>
+            <h2>
+                Please login to bring up your list!!
+            </h2>
+        </div>
+    );
+    
+}
+
+export default Home;
